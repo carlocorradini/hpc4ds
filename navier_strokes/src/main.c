838251eaf4955a01a67ab08d6e23b7e4f71f8263
Carlo <carlo.ita98@gmail.com>
2021-04-27 17:43:41 +0200

    fix

--- navier_strokes/src/main.c
+++ navier_strokes/src/main.c
@@ -3,32 +3,35 @@
 #include <mpi.h>
 #include "navier_stokes.h"
 
+#define WORLD_WIDTH 80
+#define WORLD_HEIGHT 80
+#define TICKS 50
+
 int main(void) {
-    navier_stokes_t *ns = ns_create(80, 80,
+    navier_stokes_t *ns = ns_create(WORLD_WIDTH, WORLD_HEIGHT,
                                     0.0001, 10, 0.0001,
-                                    2);
+                                    0.01);
 
     increase_density(ns, 41, 41);
-    tick(ns);
     increase_density(ns, 65, 20);
     increase_density(ns, 15, 20);
-    tick(ns);
 
-    const double **world = get_world(ns);
+    FILE *fp = fopen("output.txt", "w");
+    fprintf(fp, "x, y, d\n");
+    for (size_t i = 0; i < TICKS + 1; ++i) {
+        if (i != 0) tick(ns);
 
-    FILE *fp;
-    fp = fopen("output.txt", "w");
-    for (size_t y = 0; y < 80 + 2; ++y) {
-        for (size_t x = 0; x < 80 + 2; ++x) {
-            double density = (int) (255 * world[y][x]);
-            if (density >= 255) {
-                density = 255;
+        for (size_t y = 0; y < WORLD_HEIGHT + 2; ++y) {
+            for (size_t x = 0; x < WORLD_WIDTH + 2; ++x) {
+                const double **world = get_world(ns);
+                double density = (int) (255 * world[y][x]);
+                if (density >= 255) {
+                    density = 255;
+                }
+
+                fprintf(fp, "%ld, %ld, %d\n", x, y, (int) density);
             }
-
-            printf(" %3d ", (int) density);
-            fprintf(fp, "%d ", (int) density);
         }
-        printf("\n");
         fprintf(fp, "\n");
     }
     fclose(fp);
