cf4aee9a208c2ec1974606b10f4b8d9073a519e9
Carlo <carlo.ita98@gmail.com>
2021-05-22 19:55:47 +0200

    PARSING: Added parsing json input file. Must be better divided into different files and not test.h

--- navier_strokes/src/main.c
+++ navier_strokes/src/main.c
@@ -1,57 +1,78 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "navier_stokes.h"
+#include "test.h"
 
-#define WORLD_WIDTH 100
-#define WORLD_HEIGHT 100
-#define FLUID_VISCOSITY 0.0001
-#define FLUID_DENSITY 10
-#define FLUID_DIFFUSION 0.0001
+static ns_parse_simulation_mod_t *
+find_mod_by_tick(const ns_parse_simulation_t *const simulation, u_int64_t tick) {
+    if (simulation == NULL || simulation->mods == NULL
+        || tick < 0 || tick > simulation->ticks - 1)
+        return NULL;
 
-#define NS_TIME_STEP 0.01
-#define NS_TICKS 50  // Produces NS_TICKS + 1 in the final output
+    for (u_int64_t i = 0; i < simulation->mods_length; ++i) {
+        ns_parse_simulation_mod_t *mod = simulation->mods[i];
+
+        if (mod->tick == tick) return mod;
+    }
+
+    return NULL;
+}
 
 int main(void) {
-    // World definition
-    ns_t *ns = ns_create(WORLD_WIDTH, WORLD_HEIGHT,
-                         FLUID_VISCOSITY, FLUID_DENSITY, FLUID_DIFFUSION,
-                         NS_TIME_STEP);
+    const char *string = "{\"simulations\":[{\"time_step\":0.01,\"ticks\":50,\"world\":{\"width\":100,\"height\":100},\"fluid\":{\"viscosity\":0.0001,\"density\":10,\"diffusion\":0.0001},\"mods\":[{\"tick\":0,\"densities\":[{\"x\":41,\"y\":41},{\"x\":65,\"y\":20},{\"x\":15,\"y\":20}],\"forces\":[{\"x\":41,\"y\":41,\"velocity\":{\"x\":0,\"y\":80}}]}]}]}";
 
-    ns_increase_density(ns, 41, 41);
-    ns_increase_density(ns, 65, 20);
-    ns_increase_density(ns, 15, 20);
-    ns_apply_force(ns, 41, 41, 0, 80);
-    // END
+    ns_parse_simulations_t *simulations = ns_parse_simulations(string);
 
-    // PRINT
-    FILE *fp = fopen("output.txt", "w");
-    ns_world_t *world = ns_get_world(ns);
-    for (size_t i = 0; i < NS_TICKS + 1; ++i) {
-        if (i != 0) ns_tick(ns);
+    for (u_int64_t i_s = 0; i_s < simulations->simulations_length; ++i_s) {
+        FILE *fp = NULL;
+        ns_parse_simulation_t *simulation = NULL;
+        ns_t *ns = NULL;
+        ns_world_t *world = NULL;
 
-        for (size_t y = 0; y < world->world_height_bounds; ++y) {
-            for (size_t x = 0; x < world->world_width_bounds; ++x) {
-                // Normalized Y used to invert the origin in bottom-left and not top-right
-                size_t nY = world->world_height_bounds - 1 - y;
+        fp = fopen("output.txt", "w");
+        simulation = simulations->simulations[i_s];
+        ns = ns_create(simulation->world.width, simulation->world.height,
+                       simulation->fluid.viscosity, simulation->fluid.density, simulation->fluid.diffusion,
+                       simulation->time_step);
+        world = ns_get_world(ns);
 
-                int density = (int) (255 * *world->world[y][x].density);
-                if (density >= 255) density = 255;
+        for (u_int64_t tick = 0; tick < simulation->ticks; ++tick) {
+            const ns_parse_simulation_mod_t *const mod = find_mod_by_tick(simulation, tick);
 
-                // To see the ASCII grid
-                //fprintf(fp, "%3d ", (int) density);
-                
-                // To print in output.txt correctly
-                fprintf(fp, "%ld, %ld, %d\n", x, nY, (int) density);
+            if (mod != NULL) {
+                for (u_int64_t i_d = 0; i_d < mod->densities_length; ++i_d) {
+                    const ns_parse_simulation_mods_density_t *const density = mod->densities[i_d];
+                    ns_increase_density(ns, density->x, density->y);
+                }
+
+                for (u_int64_t i_f = 0; i_f < mod->forces_length; ++i_f) {
+                    const ns_parse_simulation_mods_force_t *const force = mod->forces[i_f];
+                    ns_apply_force(ns, force->x, force->y, force->velocity.x, force->velocity.y);
+                }
+            }
+
+            if (tick != 0) ns_tick(ns);
+
+            for (size_t y = 0; y < world->world_height_bounds; ++y) {
+                for (size_t x = 0; x < world->world_width_bounds; ++x) {
+                    // Normalized Y used to invert the origin in bottom-left and not top-right
+                    size_t nY = world->world_height_bounds - 1 - y;
+
+                    int density = (int) (255 * *world->world[y][x].density);
+                    if (density >= 255) density = 255;
+
+                    // To print in output.txt correctly
+                    fprintf(fp, "%ld, %ld, %d\n", x, nY, density);
+                }
             }
         }
-        // To see the ASCII grid
-        // fprintf(fp, "\n");
-    }
-    // END
 
-    fclose(fp);
-    ns_free_world(world);
-    ns_free(ns);
+        fclose(fp);
+        ns_free_world(world);
+        ns_free(ns);
+    }
+
+    ns_parse_simulations_free(simulations);
 
     return EXIT_SUCCESS;
 }
---
+++ navier_strokes/src/test.h
@@ -0,0 +1,361 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <cJSON.h>
+#include "navier_stokes.h"
+
+typedef struct ns_parse_simulation_world_t {
+    u_int64_t width;
+    u_int64_t height;
+} ns_parse_simulation_world_t;
+
+typedef struct ns_parse_simulation_fluid_t {
+    double viscosity;
+    double density;
+    double diffusion;
+} ns_parse_simulation_fluid_t;
+
+typedef struct ns_parse_simulation_mods_density_t {
+    u_int64_t x;
+    u_int64_t y;
+} ns_parse_simulation_mods_density_t;
+
+typedef struct ns_parse_simulation_mods_forces_velocity_t {
+    double x;
+    double y;
+} ns_parse_simulation_mods_forces_velocity_t;
+
+typedef struct ns_parse_simulation_mods_force_t {
+    u_int64_t x;
+    u_int64_t y;
+    ns_parse_simulation_mods_forces_velocity_t velocity;
+} ns_parse_simulation_mods_force_t;
+
+typedef struct ns_parse_simulation_mod_t {
+    u_int64_t tick;
+    ns_parse_simulation_mods_density_t **densities;
+    u_int64_t densities_length;
+    ns_parse_simulation_mods_force_t **forces;
+    u_int64_t forces_length;
+} ns_parse_simulation_mod_t;
+
+typedef struct ns_parse_simulation_t {
+    double time_step;
+    u_int64_t ticks;
+
+    // World
+    ns_parse_simulation_world_t world;
+
+    // Fluid
+    ns_parse_simulation_fluid_t fluid;
+
+    // Mods
+    ns_parse_simulation_mod_t **mods;
+    u_int64_t mods_length;
+} ns_parse_simulation_t;
+
+typedef struct ns_parse_simulations_t {
+    ns_parse_simulation_t **simulations;
+    u_int64_t simulations_length;
+} ns_parse_simulations_t;
+
+static bool ns_parse_simulation_check_and_assign_time_step(const cJSON *const time_step_json, double *time_step) {
+    if (time_step_json == NULL || time_step == NULL) return false;
+
+    if (!(cJSON_IsNumber(time_step_json) && time_step_json->valuedouble > 0.0))
+        return false;
+
+    *time_step = time_step_json->valuedouble;
+
+    return true;
+}
+
+static bool ns_parse_simulation_check_and_assign_ticks(const cJSON *const ticks_json, u_int64_t *ticks) {
+    if (ticks_json == NULL || ticks == NULL) return false;
+
+    if (!(cJSON_IsNumber(ticks_json) && ticks_json->valueint > 0))
+        return false;
+
+    *ticks = (u_int64_t) ticks_json->valueint;
+
+    return true;
+}
+
+static bool
+ns_parse_simulation_check_and_assign_world(const cJSON *const world_json, ns_parse_simulation_world_t *world) {
+    if (world_json == NULL || world == NULL) return false;
+
+    const cJSON *width_json = NULL;
+    const cJSON *height_json = NULL;
+
+    width_json = cJSON_GetObjectItemCaseSensitive(world_json, "width");
+    height_json = cJSON_GetObjectItemCaseSensitive(world_json, "height");
+
+    if (!(cJSON_IsNumber(width_json) && width_json->valueint > 0
+          && cJSON_IsNumber(height_json) && height_json->valueint > 0
+    ))
+        return false;
+
+    world->width = (u_int64_t) width_json->valueint;
+    world->height = (u_int64_t) height_json->valueint;
+
+    return true;
+}
+
+static bool
+ns_parse_simulation_check_and_assign_fluid(const cJSON *const fluid_json, ns_parse_simulation_fluid_t *fluid) {
+    if (fluid_json == NULL || fluid == NULL) return false;
+
+    const cJSON *viscosity_json = NULL;
+    const cJSON *density_json = NULL;
+    const cJSON *diffusion_json = NULL;
+
+    viscosity_json = cJSON_GetObjectItemCaseSensitive(fluid_json, "viscosity");
+    density_json = cJSON_GetObjectItemCaseSensitive(fluid_json, "density");
+    diffusion_json = cJSON_GetObjectItemCaseSensitive(fluid_json, "diffusion");
+
+    if (!(cJSON_IsNumber(viscosity_json) && viscosity_json->valuedouble > 0
+          && cJSON_IsNumber(density_json) && density_json->valuedouble > 0
+          && cJSON_IsNumber(diffusion_json) && diffusion_json->valuedouble > 0
+    ))
+        return false;
+
+    fluid->viscosity = viscosity_json->valuedouble;
+    fluid->density = density_json->valuedouble;
+    fluid->diffusion = diffusion_json->valuedouble;
+
+    return true;
+}
+
+static bool ns_parse_simulation_check_and_assign_mod(const cJSON *const mod_json, ns_parse_simulation_mod_t *mod) {
+    if (mod_json == NULL || mod == NULL) return false;
+
+    const cJSON *tick_json = NULL;
+    const cJSON *densities_json = NULL;
+    const cJSON *forces_json = NULL;
+
+    tick_json = cJSON_GetObjectItemCaseSensitive(mod_json, "tick");
+    densities_json = cJSON_GetObjectItemCaseSensitive(mod_json, "densities");
+    forces_json = cJSON_GetObjectItemCaseSensitive(mod_json, "forces");
+
+    if (!(cJSON_IsNumber(tick_json) && tick_json->valueint >= 0
+          && (cJSON_IsNull(densities_json) || cJSON_IsArray(densities_json))
+          && (cJSON_IsNull(forces_json) || cJSON_IsArray(forces_json))
+    ))
+        return false;
+
+    mod->tick = (u_int64_t) tick_json->valueint;
+
+    if (cJSON_IsNull(densities_json)) {
+        mod->densities_length = 0;
+        mod->densities = NULL;
+    } else {
+        const cJSON *density_json = NULL;
+
+        mod->densities_length = (u_int64_t) cJSON_GetArraySize(densities_json);
+        mod->densities = (ns_parse_simulation_mods_density_t **) calloc(mod->densities_length,
+                                                                        sizeof(ns_parse_simulation_mods_density_t *));
+        u_int64_t index = 0;
+        cJSON_ArrayForEach(density_json, densities_json) {
+            ns_parse_simulation_mods_density_t *density = NULL;
+            const cJSON *x = NULL;
+            const cJSON *y = NULL;
+
+            density = (ns_parse_simulation_mods_density_t *) malloc(sizeof(ns_parse_simulation_mods_density_t));
+            if (density == NULL) return false;
+
+            x = cJSON_GetObjectItemCaseSensitive(density_json, "x");
+            y = cJSON_GetObjectItemCaseSensitive(density_json, "y");
+
+            if (!(cJSON_IsNumber(x) && x->valueint >= 0
+                  && cJSON_IsNumber(y) && y->valueint >= 0
+            ))
+                return false;
+
+            density->x = (u_int64_t) x->valueint;
+            density->y = (u_int64_t) y->valueint;
+
+            mod->densities[index] = density;
+            index += 1;
+        }
+    }
+
+    if (cJSON_IsNull(forces_json)) {
+        mod->forces_length = 0;
+        mod->forces = NULL;
+    } else {
+        const cJSON *force_json = NULL;
+
+        mod->forces_length = (u_int64_t) cJSON_GetArraySize(forces_json);
+        mod->forces = (ns_parse_simulation_mods_force_t **) calloc(mod->forces_length,
+                                                                   sizeof(ns_parse_simulation_mods_force_t *));
+        u_int64_t index = 0;
+        cJSON_ArrayForEach(force_json, forces_json) {
+            ns_parse_simulation_mods_force_t *force = NULL;
+            const cJSON *x = NULL;
+            const cJSON *y = NULL;
+            const cJSON *velocity = NULL;
+            const cJSON *velocity_x = NULL;
+            const cJSON *velocity_y = NULL;
+
+            force = (ns_parse_simulation_mods_force_t *) malloc(sizeof(ns_parse_simulation_mods_force_t));
+            if (force == NULL) return false;
+
+            velocity = cJSON_GetObjectItemCaseSensitive(force_json, "velocity");
+            if (!cJSON_IsObject(velocity)) return false;
+
+            x = cJSON_GetObjectItemCaseSensitive(force_json, "x");
+            y = cJSON_GetObjectItemCaseSensitive(force_json, "y");
+            velocity_x = cJSON_GetObjectItemCaseSensitive(velocity, "x");
+            velocity_y = cJSON_GetObjectItemCaseSensitive(velocity, "y");
+
+            if (!(cJSON_IsNumber(x) && x->valueint >= 0
+                  && cJSON_IsNumber(y) && y->valueint >= 0
+                  && cJSON_IsNumber(velocity_x) && velocity_x->valuedouble >= 0
+                  && cJSON_IsNumber(velocity_y) && velocity_y->valuedouble >= 0
+            ))
+                return false;
+
+            force->x = (u_int64_t) x->valueint;
+            force->y = (u_int64_t) y->valueint;
+            force->velocity.x = velocity_x->valuedouble;
+            force->velocity.y = velocity_y->valuedouble;
+
+            mod->forces[index] = force;
+            index += 1;
+        }
+    }
+
+    return true;
+}
+
+static bool ns_parse_simulation_check_and_assign_mods(const cJSON *const mods_json, ns_parse_simulation_t *simulation) {
+    if (simulation == NULL) return false;
+    if (mods_json == NULL) {
+        simulation->mods_length = 0;
+        simulation->mods = NULL;
+        return true;
+    }
+    if (!cJSON_IsArray(mods_json)) return false;
+
+    const cJSON *mod_json = NULL;
+
+    simulation->mods_length = (u_int64_t) cJSON_GetArraySize(mods_json);
+    simulation->mods = (ns_parse_simulation_mod_t **) calloc(simulation->mods_length,
+                                                             sizeof(ns_parse_simulation_mod_t *));
+    if (simulation->mods == NULL) return false;
+
+    u_int64_t index = 0;
+    cJSON_ArrayForEach(mod_json, mods_json) {
+        ns_parse_simulation_mod_t *mod = NULL;
+
+        mod = (ns_parse_simulation_mod_t *) malloc(sizeof(ns_parse_simulation_mod_t));
+        if (mod == NULL) return false;
+
+        if (!ns_parse_simulation_check_and_assign_mod(mod_json, mod)) return false;
+
+        simulation->mods[index] = mod;
+
+        index += 1;
+    }
+
+    return true;
+}
+
+void ns_parse_simulation_free(ns_parse_simulation_t *simulation) {
+    if (simulation != NULL && simulation->mods != NULL) {
+        for (u_int64_t i_m = 0; i_m < simulation->mods_length; ++i_m) {
+            ns_parse_simulation_mod_t *mod = simulation->mods[i_m];
+
+            if (mod != NULL && mod->densities != NULL) {
+                for (u_int64_t i_d = 0; i_d < mod->densities_length; ++i_d) {
+                    ns_parse_simulation_mods_density_t *density = mod->densities[i_d];
+                    free(density);
+                }
+                free(mod->densities);
+            }
+
+            if (mod != NULL && mod->forces != NULL) {
+                for (u_int64_t i_f = 0; i_f < mod->densities_length; ++i_f) {
+                    ns_parse_simulation_mods_force_t *force = mod->forces[i_f];
+                    free(force);
+                }
+                free(mod->forces);
+            }
+
+            free(mod);
+        }
+
+        free(simulation->mods);
+    }
+
+    free(simulation);
+}
+
+void ns_parse_simulations_free(ns_parse_simulations_t *simulations) {
+    if (simulations != NULL && simulations->simulations != NULL) {
+        for (u_int64_t i_s = 0; i_s < simulations->simulations_length; ++i_s) {
+            ns_parse_simulation_free(simulations->simulations[i_s]);
+        }
+
+        free(simulations->simulations);
+    }
+
+    free(simulations);
+}
+
+static void *ns_parse_simulations_error(cJSON *file_json, ns_parse_simulations_t *simulations) {
+    cJSON_Delete(file_json);
+    ns_parse_simulations_free(simulations);
+
+    return NULL;
+}
+
+ns_parse_simulations_t *ns_parse_simulations(const char *const file) {
+    ns_parse_simulations_t *simulations = NULL;
+    cJSON *file_json = NULL;
+    const cJSON *simulation_json = NULL;
+    const cJSON *simulations_json = NULL;
+
+    simulations = (ns_parse_simulations_t *) malloc(sizeof(ns_parse_simulations_t));
+    if (simulations == NULL) return ns_parse_simulations_error(file_json, simulations);
+
+    file_json = cJSON_Parse(file);
+    if (file_json == NULL) return ns_parse_simulations_error(file_json, simulations);
+
+    simulations_json = cJSON_GetObjectItemCaseSensitive(file_json, "simulations");
+    if (!cJSON_IsArray(simulations_json)) return ns_parse_simulations_error(file_json, simulations);
+
+    simulations->simulations_length = (u_int64_t) cJSON_GetArraySize(simulations_json);
+    simulations->simulations = (ns_parse_simulation_t **) calloc(simulations->simulations_length,
+                                                                 sizeof(ns_parse_simulation_t *));
+    if (simulations->simulations == NULL) return ns_parse_simulations_error(file_json, simulations);
+
+    u_int64_t index = 0;
+    cJSON_ArrayForEach(simulation_json, simulations_json) {
+        ns_parse_simulation_t *simulation = NULL;
+
+        simulation = (ns_parse_simulation_t *) malloc(sizeof(ns_parse_simulation_t));
+        if (simulation == NULL) return ns_parse_simulations_error(file_json, simulations);
+
+        // Check & Assign
+        if (!(ns_parse_simulation_check_and_assign_time_step(
+                cJSON_GetObjectItemCaseSensitive(simulation_json, "time_step"), &simulation->time_step)
+              && ns_parse_simulation_check_and_assign_ticks(
+                cJSON_GetObjectItemCaseSensitive(simulation_json, "ticks"), &simulation->ticks)
+              && ns_parse_simulation_check_and_assign_world(
+                cJSON_GetObjectItemCaseSensitive(simulation_json, "world"), &simulation->world)
+              && ns_parse_simulation_check_and_assign_fluid(
+                cJSON_GetObjectItemCaseSensitive(simulation_json, "fluid"), &simulation->fluid)
+              && ns_parse_simulation_check_and_assign_mods(
+                cJSON_GetObjectItemCaseSensitive(simulation_json, "mods"), simulation)
+        ))
+            return ns_parse_simulations_error(file_json, simulations);
+
+        simulations->simulations[index] = simulation;
+        index += 1;
+    }
+
+    cJSON_Delete(file_json);
+    return simulations;
+}
