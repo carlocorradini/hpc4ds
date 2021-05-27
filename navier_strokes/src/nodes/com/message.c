5331ca90baffea26e8c9ea88980ca16fb9b677e2
Carlo <carlo.ita98@gmail.com>
2021-05-27 18:32:50 +0200

    FIX: fix offset?

--- navier_strokes/src/nodes/com/message.c
+++ navier_strokes/src/nodes/com/message.c
@@ -3,14 +3,26 @@
 void com_message_MPI_datatype(MPI_Datatype *message_type) {
     if (message_type == NULL) return;
 
+    // Number of items
     enum { n_items = 2 };
-    int block_lengths[n_items] = {1, 1};
-    MPI_Datatype types[n_items] = {MPI_C_BOOL, MPI_UINT64_T};
-    MPI_Aint offsets[n_items] = {
-            offsetof(com_message_t, terminate),
-            offsetof(com_message_t, simulation_id)
-    };
 
+    // How many elements for each item
+    int block_lengths[n_items] = {1, 1};
+
+    // Type of each item
+    MPI_Datatype types[n_items] = {MPI_C_BOOL, MPI_UINT64_T};
+
+    // Calculate offsets
+    MPI_Aint offsets[n_items];
+    struct com_message_t m;
+    MPI_Aint base_address;
+    MPI_Get_address(&m, &base_address);
+    MPI_Get_address(&m.terminate, &offsets[0]);
+    MPI_Get_address(&m.simulation_id, &offsets[1]);
+    offsets[0] = MPI_Aint_diff(offsets[0], base_address);
+    offsets[1] = MPI_Aint_diff(offsets[1], base_address);
+
+    // Create the struct type
     MPI_Type_create_struct(n_items, block_lengths, offsets, types, message_type);
     MPI_Type_commit(message_type);
 }
