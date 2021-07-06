44f8466234f6cb6bff44853358a1c623d8467a51
massimilianofronza <maxfronza97@gmail.com>
2021-07-06 16:30:08 +0200

    Pbs scripts for the cluster tests

--- navier_strokes/hpc/navierstokes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#PBS -l nodes=8:ppn=1
-#PBS -q short_cpuQ
-
-readonly NUMBER_PROCESSES=8
-
-module load mpich-3.2
-mpirun.actual -np "${NUMBER_PROCESSES}" ./navierstokes --simulations=./simulation_wall.json --results=./results
