c08a755746a419e505f8381f341341f146842099
massimilianofronza <maxfronza97@gmail.com>
2021-07-08 16:46:05 +0200

    Test pbs script

---
+++ navier_strokes/hpc/pbs/navierstokes_LOC_TESTING_1_node_1_worker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#PBS -l nodes=1
+#PBS -q short_cpuQ
+
+# Current working directory
+# See https://unix.stackexchange.com/questions/207205/current-directory-in-qsub
+cd "$PBS_O_WORKDIR" || exit $?
+
+readonly NUMBER_PROCESSES=2
+
+module load mpich-3.2
+mpirun.actual -np "${NUMBER_PROCESSES}" ../navierstokes --simulations=../simulations.json --results=../results
